c6ec37b4933892d734dd9f80eed142c38278f3af
Rafael Tavares <rafael.tawares@gmail.com>
2021-09-13 01:13:42 -0300

    Backend

---
+++ backend/src/routes.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const IncidentController = require("./controllers/IncidentController");
+const OngController = require("./controllers/OngController");
+const ProfileController = require("./controllers/ProfileController");
+const SessionController = require("./controllers/SessionController");
+
+const routes = express.Router();
+
+routes.get("/incidents", IncidentController.index);
+routes.post("/incidents", IncidentController.create);
+routes.delete("/incidents/:id", IncidentController.delete);
+
+routes.get("/ongs", OngController.index);
+routes.post("/ongs", OngController.create);
+
+routes.get("/profile", ProfileController.index);
+
+routes.post("/sessions", SessionController.create);
+
+module.exports = routes;
+
+/**
+ * Rota / Recurso
+ */
+
+/**
+ * Métodos HTTP
+ *
+ * GET: Buscar/listar uma informação do backend
+ * POST: Criar uma informação no backend
+ * PUT: Alterar uma informação no backend
+ * DELETE: Deletar uma informação no backend
+ */
+
+/**
+ * Tipos de parâmetros
+ *
+ * Query Params: Parâmetros nomeados enviados na rota "?" (filtro, paginação)
+ * Route Params: Parâmetros utilizados para identificar recursos
+ * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+ */
+
+/**
+ * SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server
+ * NoSQL: MongoDB, CouchDB, etc
+ */
+
+/**
+ * Driver: SELECT * FROM users
+ * Query Builder: table('users').select('*').where()
+ */
